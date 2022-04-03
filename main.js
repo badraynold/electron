3cf76dfed2d3526e928e6909b8d09332ff68616f
Rafał Łukawski <rs.lukawski@gmail.com>
2022-04-03 14:49:36 +0200

    Add icons

--- main.js
+++ main.js
@@ -6,7 +6,11 @@ function createWindow() {
     title: "ImageShrink",
     width: 500,
     height: 600,
+    icon: './assets/icons/Icon_256x256.png'
   });
+  //   mainWindow.loadURL("https://generator-paskow.pl");
+  //   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile("./app/index.html");
 }
 
 app.on("ready", createWindow);
