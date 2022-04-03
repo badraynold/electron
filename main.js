9618c300f5b04756b8313aa70c8d981fadcfb8a5
Rafał Łukawski <rs.lukawski@gmail.com>
2022-04-03 15:38:49 +0200

    Menu add

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 process.env.NODE_ENV = "development";
 const isDev = process.env.NODE_ENV !== "production" ? true : false;
 console.log(process.platform);
-
+let mainWindow;
 function createWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: "ImageShrink",
     width: 500,
     height: 600,
@@ -16,7 +16,20 @@ function createWindow() {
   mainWindow.loadFile("./app/index.html");
 }
 
-app.on("ready", createWindow);
+app.on("ready", () => {
+  createWindow();
+  const mainMenu = Menu.buildFromTemplate(menu);
+  Menu.setApplicationMenu(mainMenu);
+  mainWindow.on("closed", () => (mainWindow = null));
+});
+
+const menu = [
+  {
+    label: "File",
+    submenu: [{ label: "Quit", click: () => app.quit() }],
+  },
+];
+
 app.on("window-all-closed", () => {
   if (process.platform !== "darwin") app.quit();
 });
