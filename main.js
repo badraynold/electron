f228c399c9720b7b584afa564524b008943c15a6
Rafał Łukawski <rs.lukawski@gmail.com>
2022-04-03 15:55:05 +0200

    menu

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require("electron");
+const { app, BrowserWindow, Menu, globalShortcut } = require("electron");
 process.env.NODE_ENV = "development";
 const isDev = process.env.NODE_ENV !== "production" ? true : false;
 console.log(process.platform);
@@ -10,6 +10,7 @@ function createWindow() {
     height: 600,
     icon: `{$__dirname}/assets/icons/Icon_256x256.png`,
     resizable: false,
+    backgroundColor: "white",
   });
   //   mainWindow.loadURL("https://generator-paskow.pl");
   //   mainWindow.loadURL(`file://${__dirname}/app/index.html`);
@@ -20,13 +21,22 @@ app.on("ready", () => {
   createWindow();
   const mainMenu = Menu.buildFromTemplate(menu);
   Menu.setApplicationMenu(mainMenu);
+
+  globalShortcut.register("CmdOrCtrl+R", () => {
+    mainWindow.reload();
+  });
+  globalShortcut.register("Ctrl+Shift+I", () => {
+    mainWindow.toggleDevTools();
+  });
   mainWindow.on("closed", () => (mainWindow = null));
 });
 
 const menu = [
   {
     label: "File",
-    submenu: [{ label: "Quit", click: () => app.quit() }],
+    submenu: [
+      { label: "Quit", accelerator: "CmdOrCtrl+W", click: () => app.quit() },
+    ],
   },
 ];
 
